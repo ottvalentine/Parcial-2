6314beeac6c027068d444fce7fc2a64bd546001f
Valentine Otto <valentine.otto@escola.pr.gov.br>
2026-03-31 19:50:43 -0300

    Add files via upload

---
+++ desafio5.py
@@ -0,0 +1,13 @@
+print("=== ÁREA DO TRIÂNGULO ===\n")
+
+print("Passo 1: Digite o valor da base")
+base = float(input("→ Base: "))
+
+print("\nPasso 2: Digite o valor da altura")
+altura = float(input("→ Altura: "))
+
+print("\nPasso 3: Calculando...")
+
+area = (base * altura) / 2
+
+print(f"\nResultado: A área do triângulo é {area}")
